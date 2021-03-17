841117e338f3737b96b7037d203cb748569aa9aa
wangxh86 <71023386+wangxh86@users.noreply.github.com>
2021-03-18 02:42:42 +0800

    Update iboxpayCOOKIE.js

--- Task/iboxpayCOOKIE.js
+++ Task/iboxpayCOOKIE.js
@@ -3,15 +3,11 @@
 //独立COOKIE文件     ck在``里面填写，多账号换行
 
 let refreshtokenVal= ``
-let iboxpayvideoheaderVal= ``
-let iboxpayvideobodyVal= ``
 
 
 let iboxpaycookie = {
 
-  refreshtokenVal: refreshtokenVal, 
-    iboxpayvideoheaderVal: iboxpayvideoheaderVal,  
-      iboxpayvideobodyVal: iboxpayvideobodyVal,   
+  refreshtokenVal: refreshtokenVal,  
   
 }
 
